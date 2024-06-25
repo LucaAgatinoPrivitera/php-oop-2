c490dd09a76204a000118b816e247e142b7cac09
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-25 18:28:37 +0200

    Ho creato le cards e anche messo le img dinamiche che cambiano in base all'animale

--- index.php
+++ index.php
@@ -17,12 +17,20 @@ include __DIR__ . "/data.php";
 
     <!-- <?php //echo $prodottoSingolo[0]->nome 
             ?> -->
-
+<!-- <img src="/img/cane.jpg" alt=""> da chiedere perché non va? -->
     <div class="container">
         <?php foreach ($prodottoSingolo as $prodotto) {
             // echo "<div>" . "<span>" . $prodotto->nome . " </span>" . "<span>" . $prodotto->categoria . " </span>" . "<span>" . $prodotto->prezzo . " </span>" . "<span>" . $prodotto->nome  . " </span>" . "<span>" . $cuccia->nomeCuccia ." </span>" . "<span>". $cuccia->capienza . "</span>" . "</div>";
 
-            echo "<div class='cards'>" . "<span class='nome'>" . $prodotto->nome . " </span>" . "<span class='descrizione'>" . $prodotto->getDescrizione() . " </span>" ;
+            echo "<div class='cards'>";
+            if (isset($prodotto->categoria) && $prodotto->categoria === $categoryCane) {
+                echo "<img src='https://www.veterinarimatera.it/wp-content/uploads/2021/03/cane-1.jpg'>";
+            }
+            else
+            {
+                echo "<img src='https://www.elite-pet.it/media/k2/items/cache/a786836489dab4f04d53706ec376ba50_L.jpg'>";
+            }
+            echo "<span class='nome'>" . $prodotto->nome . " </span>" . "<span class='descrizione'>" . $prodotto->getDescrizione() . " </span>" ;
             if (isset($prodotto->elettrico)) {
                 echo "<span>" .  $prodotto->elettrico . " </span>";
             }
